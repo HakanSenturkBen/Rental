afa0383d00481bf3249f225b0035965cc846148b
hakan.fikret.senturk <hakan.fikret.senturk@gmail.com>
2021-08-10 14:50:38 +0300

    commit

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Business.BusinessAspects.Autofac;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Caching;
@@ -23,6 +24,7 @@ namespace Business.Concrete
             carDal = cardal;
         }
 
+        [SecuredOperation("member")]
         [ValidationAspect(typeof(CarValidator))]
         [CacheRemoveAspect("ICarService.Get")]
         public IResult Add(Car car)
